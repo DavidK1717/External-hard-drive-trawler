124d6fbc63e5d92cc4797cb0e8faf1b8c7a0c30f
DavidK1717 <dkirkman17@gmail.com>
2020-01-08 14:12:22 +0000

    Allowed for network drives

--- Filesearch/Drive.cs
+++ Filesearch/Drive.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 using MySqlDB;
@@ -21,7 +23,7 @@ namespace Filesearch
         public Drive(string driveLetter)
         {
             DriveInfo dv = new DriveInfo(driveLetter);
-            VolumeLabel = dv.VolumeLabel;
+            VolumeLabel = GetDriveName(dv);
             string totalCapacity = dv.TotalSize.ToString();
             string availableSpace = dv.AvailableFreeSpace.ToString();
             string fileSystem = dv.DriveFormat;
@@ -113,6 +115,25 @@ namespace Filesearch
                 MessageBox.Show("[" + ex.ToString() + "]", "An error occurred");
                 return false;
             }
+        }
+
+        /// <summary>
+        /// If a network drive is selected it might not have a volume label
+        /// assigned, so we need to extract a name from the UNC path.
+        /// </summary>
+        /// <param name="dv"></param>
+        /// <returns></returns>
+        public string GetDriveName(DriveInfo dv)
+        {
+            if (dv.VolumeLabel != "")
+                return dv.VolumeLabel;
+            else if (dv.DriveType == DriveType.Network)
+            {
+                var start = Pathing.GetUNCPath(dv.Name).LastIndexOf("\\") + 1;
+                return Pathing.GetUNCPath(dv.Name).Substring(start);
+            }
+            else
+                return "";
         }
 
     }  
@@ -349,6 +370,53 @@ namespace Filesearch
         public static void ExceptionHandler(Exception ex, string title)
         {
             MessageBox.Show("[" + ex.ToString() + "]", title);
+        }
+    }
+
+    /// <summary>
+    /// Code from:
+    /// https://www.c-sharpcorner.com/uploadfile/sri85kanth/accessing-network-drive-in-C-Sharp/
+    /// </summary>
+    public static class Pathing
+    {
+        [DllImport("mpr.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int WNetGetConnection(
+            [MarshalAs(UnmanagedType.LPTStr)] string localName,
+            [MarshalAs(UnmanagedType.LPTStr)] StringBuilder remoteName,
+            ref int length);
+        /// <summary>
+        /// Given a path, returns the UNC path or the original. (No exceptions
+        /// are raised by this function directly). For example, "P:\2008-02-29"
+        /// might return: "\\networkserver\Shares\Photos\2008-02-09"
+        /// </summary>
+        /// <param name="originalPath">The path to convert to a UNC Path</param>
+        /// <returns>A UNC path. If a network drive letter is specified, the
+        /// drive letter is converted to a UNC or network path. If the
+        /// originalPath cannot be converted, it is returned unchanged.</returns>
+        public static string GetUNCPath(string originalPath)
+        {
+            StringBuilder sb = new StringBuilder(512);
+            int size = sb.Capacity;
+            // look for the {LETTER}: combination ...
+            if (originalPath.Length > 2 && originalPath[1] == ':')
+            {
+                // don't use char.IsLetter here - as that can be misleading
+                // the only valid drive letters are a-z && A-Z.
+                char c = originalPath[0];
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                {
+                    int error = WNetGetConnection(originalPath.Substring(0, 2),
+                        sb, ref size);
+                    if (error == 0)
+                    {
+                        DirectoryInfo dir = new DirectoryInfo(originalPath);
+                        string path = Path.GetFullPath(originalPath)
+                            .Substring(Path.GetPathRoot(originalPath).Length);
+                        return Path.Combine(sb.ToString().TrimEnd(), path);
+                    }
+                }
+            }
+            return originalPath;
         }
     }
 }
